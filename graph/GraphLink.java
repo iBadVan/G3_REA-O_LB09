74af01eae8a84850e8411c50d1d038548a7f649f
iBadVan <ivan.edgardo.reso@gmail.com>
2025-06-10 15:28:02 -0500

    Update GraphLink.java

--- graph/GraphLink.java
+++ graph/GraphLink.java
@@ -1,5 +1,23 @@
 package graph;
 
+import lista.ListLinked;
+
 public class GraphLink<E> {
-    
+    protected ListLinked<Vertex<E>> listVertex;
+
+    public GraphLink() {
+        listVertex = new ListLinked<Vertex<E>>();
+    }
+
+    public void insertVertex(E data) {
+        
+    }
+
+    public void insertEdge(E verOri, E verDes) {
+        
+    }
+
+    public String toString() {
+        return this.listVertex.toString();
+    }
 }
